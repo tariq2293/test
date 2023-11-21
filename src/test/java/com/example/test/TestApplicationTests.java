d409c93297706df45b632424449cc73ba064ab7c
tariq2293 <tariq@provility.com>
2023-11-21 20:12:26 +0530

    initial test

---
+++ src/test/java/com/example/test/TestApplicationTests.java
@@ -0,0 +1,17 @@
+package com.example.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
+class TestApplicationTests {
+
+    @Test
+    void contextLoads() {
+        int a = 10;
+        int b = 20;
+        assertEquals(35, a+b);
+    }
+
+}
